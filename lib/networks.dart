530b1d54484823da2ab2123770ee4a4203c9df49
amratef <amratef@gmail.com>
2025-09-27 08:50:05 -1100

    idding branch netwrk import 'package:wifi_scan/wifi_scan.dart';

--- lib/networks.dart
+++ lib/networks.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:wifi_scan/wifi_scan.dart';
 
 class NetworksPage extends StatefulWidget {
   const NetworksPage({super.key});
@@ -7,3 +7,4 @@ class NetworksPage extends StatefulWidget {
   @override
   State<NetworksPage> createState() => _NetworksPageState();
 }
+
