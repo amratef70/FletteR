ca08f08cf4602b430df94154d99e1349e62e4a5f
asd757076 <asd757076@gmail.com>
2025-09-27 10:42:40 -1100

    edding branch asdDetails  details.dart class fix

--- lib/details.dart
+++ lib/details.dart
@@ -1,2 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:wifi_scan/wifi_scan.dart';
+
+class _DetailsPageState extends State<DetailsPage> {
+  @override
+  void initState() {
+    super.initState();
+    if (widget.selectedNetwork != null) {
+      print("DetailsPage initState: Received SSID: ${widget.selectedNetwork!.ssid}");
+    } else {
+      print("DetailsPage initState: No network selected.");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (widget.selectedNetwork == null) {
+
+      return Scaffold(
+
+
+
+
+
+
+
+
+
+
+      );
+    }
+  }
+}
