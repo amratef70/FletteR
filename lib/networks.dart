e1c73fb6264935f642f48f51f11071bcae4fa636
amratef <amratef@gmail.com>
2025-09-27 08:45:20 -1100

    idding branch netwrk class NetworksPage

--- lib/networks.dart
+++ lib/networks.dart
@@ -1,1 +1,9 @@
 import 'package:flutter/material.dart';
+
+
+class NetworksPage extends StatefulWidget {
+  const NetworksPage({super.key});
+
+  @override
+  State<NetworksPage> createState() => _NetworksPageState();
+}
