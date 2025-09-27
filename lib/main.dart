488d12389f0a08dba4f902167cf7e491dc9ccd83
amratef <amratef@gmail.com>
2025-09-27 06:52:02 -1100

    adding branch main.dart  class

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,26 @@ import 'package:flutter/material.dart';
 
 
 main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+
+
+
+      home: const Directionality(
+        textDirection: TextDirection.rtl,
+
+
+        child: ,
+
+
+      ),
+
+    );
+  }
+}
